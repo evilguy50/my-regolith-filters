51ddd0a4a121c5a0cc4afd31d5f96648b53a6c36
evilguy50 <robbryan42@gmail.com>
2022-12-27 16:06:11 -0800

    Set var to let and fix typos

--- new_exec/new_exec.ts
+++ new_exec/new_exec.ts
@@ -4,9 +4,9 @@ import * as path from "path"
 import * as json from "json5"
 
 function getElemInRange(array: any, join: string,  min: number, max: number): string {
-    var result = ""
-    var num = -1
-    for (var a in array) {
+    let result = ""
+    let num = -1
+    for (let a in array) {
         num++
         if (num >= min && num <= max) {
             result += array[a] + join
@@ -28,29 +28,29 @@ function *walkSync(dir: string): any {
 const cmdTemplate = "execute as $selector positioned $posrun $cmd"
 const detectTemplate = "execute as $selector positioned $posif block $2pos$block $data run $cmd"
 function detectExec(args: Array<string>, selector: string) {
-        var pos = getElemInRange(args, " ", 2, 4)
-        var pos2 = getElemInRange(args, " ", 6, 8)
-        var block = args[9]
-        var data = args[10]
-        var new_cmd = getElemInRange(args, " ", 11, args.length - 1)
-        var newL = detectTemplate.replace("$selector", selector).replace("$pos", pos).replace("$2pos", pos2).replace("$block", block).replace("$data", data).replace("$cmd", new_cmd)
+        let pos = getElemInRange(args, " ", 2, 4)
+        let pos2 = getElemInRange(args, " ", 6, 8)
+        let block = args[9]
+        let data = args[10]
+        let new_cmd = getElemInRange(args, " ", 11, args.length - 1)
+        let newL = detectTemplate.replace("$selector", selector).replace("$pos", pos).replace("$2pos", pos2).replace("$block", block).replace("$data", data).replace("$cmd", new_cmd)
         return [newL, new_cmd]
 }
 for (const filePath of walkSync("./BP/functions")) {
     let f = fs.readFileSync(filePath, "utf8")
     let fStr = f.split("\n")
-    for (var l in fStr) {
-        var cmd = cmds.Command.parse(fStr[l])
+    for (let l in fStr) {
+        let cmd = cmds.Command.parse(fStr[l])
         if (cmd.getKeyword() == "execute") {
-            var cmdStr = JSON.stringify(cmd)
-            var cmdJson = json.parse(cmdStr)
-            var args: Array<string> = []
-            for (var c in cmdJson["parameters"]) {
+            let cmdStr = JSON.stringify(cmd)
+            let cmdJson = JSON.parse(cmdStr)
+            let args: Array<string> = []
+            for (let c in cmdJson["parameters"]) {
                 args.push(cmdJson["parameters"][c]["text"])
             }
-            var detect = false
+            let detect = false
             let params = ["as", "at", "run", "if", "unless", "positioned"]
-            for (var d in params) {
+            for (let d in params) {
                 if (args[1].startsWith(params[d])) {
                     detect = true
                 }
@@ -59,10 +59,10 @@ for (const filePath of walkSync("./BP/functions")) {
                 console.log(`command already new execute: skipping`)
             }
             if (detect == false) {
-                var selector = args[1]
-                var pos = getElemInRange(args, " ", 2, 4)
-                var new_cmd: string
-                var newL: string
+                let selector = args[1]
+                let pos = getElemInRange(args, " ", 2, 4)
+                let new_cmd: string
+                let newL: string
                 if (args[5] != "detect") {
                     new_cmd = getElemInRange(args, " ", 5, args.length - 1)
                     newL = cmdTemplate.replace("$selector", selector).replace("$pos", pos).replace("$cmd", new_cmd)
@@ -72,22 +72,22 @@ for (const filePath of walkSync("./BP/functions")) {
                 }
                 if (new_cmd.startsWith("execute")) {
                     while (new_cmd.startsWith("execute")) {
-                        var aCmd = cmds.Command.parse(new_cmd)
-                        var aCmdStr = JSON.stringify(aCmd)
-                        var aCmdJson = json.parse(aCmdStr)
-                        var nArgs: Array<string> = []
-                        var a_cmd = ""
-                        for (var c in aCmdJson["parameters"]) {
+                        let aCmd = cmds.Command.parse(new_cmd)
+                        let aCmdStr = JSON.stringify(aCmd)
+                        let aCmdJson = json.parse(aCmdStr)
+                        let nArgs: Array<string> = []
+                        let a_cmd = ""
+                        for (let c in aCmdJson["parameters"]) {
                             nArgs.push(aCmdJson["parameters"][c]["text"])
                         }
                         if (nArgs[5] != "detect") {
-                            var new_selector = nArgs[1]
-                            var new_pos = getElemInRange(nArgs, " ", 2, 4)
+                            let new_selector = nArgs[1]
+                            let new_pos = getElemInRange(nArgs, " ", 2, 4)
                             a_cmd = getElemInRange(nArgs, " ", 5, nArgs.length - 1)
                             newL = newL.replace(new_cmd, cmdTemplate.replace("$selector", new_selector).replace("$pos", new_pos).replace("$cmd", a_cmd))
                         } else {
-                            var new_selector = nArgs[1]
-                            var execOut = detectExec(nArgs, new_selector)
+                            let new_selector = nArgs[1]
+                            let execOut = detectExec(nArgs, new_selector)
                             newL = newL.replace(new_cmd, execOut[0])
                             a_cmd = execOut[1]
                         }
